f2902577ea1c4f77d2d3c4b1d9801b711ed4a84e
SourabhDeqode <sogupta@deqode.com>
2022-01-19 09:20:13 +0530

    issue of 10000 results solved

--- query.js
+++ query.js
@@ -8,7 +8,7 @@ const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io
 const erc721 = require("@0xcert/ethereum-erc721/build/erc721.json").ERC721;
 
 const connectDB = require('./models/connection');
-//connectDB();
+connectDB();
 
 function ContractDetails (){
 for(let i=0; i<1; i++){
